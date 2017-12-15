42cfa38b2dc67036f6e6c760be870cf8089bc558
Ismayil Baghirov <ibaghirov2019@ada.edu.az>
2017-12-15 22:28:01 +0400

    (Ismayil Mansirov) Contact Us Page

--- wwww/views/site/contact.php
+++ wwww/views/site/contact.php
@@ -5,11 +5,19 @@
         <div class="row">
 
             <div class="col-lg-6">
-                <h4>Contact Us Information</h4>
+                <h4>How can pros contact Fresh Shine?</h4>
+                <p>For urgent issues you can write an email to us. Here is the email:</p> 
+                <p>contactfreshandshine@gmail.com</p>
+<p>We also have phone number:</p>
+<p>+994 51 322 22 22</p>
+      <p>012 444 22 22</p>
 
-                <br/>
+     <h4> Need help? Here email: helpfreshandshine@gmail.com  — For urgent issues, we aim to respond within minutes. </h4>
 
-                <p>Here will be Contact Us information</p>
+<p>You can also write to us at: </p>
+<p>Attn: Customer Service, Fresh and Shine Technologies, Inc. </p>
+<p>P.O. Box 1122, Baki, Genclik 10159</p>
+
             </div>
         </div>
     </div>
